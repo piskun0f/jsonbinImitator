bc493f8a76ec44a24653ea10bd54c7fb344b4206
Андрей Пискунов <adpiskunov@miem.hse.ru>
2022-06-17 23:30:59 +0300

    ✨ Add save data on disk

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const fs = require('fs')
+
+const FILE_NAME = "data";
 const app = express()
 const PORT = 3000
 
+function saveData() {
+  fs.writeFileSync(FILE_NAME, JSON.stringify(data), { encodin: 'utf-8' })
+}
+
+function readData() {
+  if (fs.existsSync(FILE_NAME)) {
+    data = JSON.parse(fs.readFileSync(FILE_NAME, { encoding: 'utf-8' }))
+  }
+}
+
 let data = {};
+readData();
 
 const AUTH_HEADER = "X-Master-Key".toLowerCase();
 const API_TOKEN = "$2b$10$UghmBJDa./wqWUTjc.B96e9Q1w7mMRChJAMCSnCFjQNwcVNyDq5QS";
@@ -26,6 +40,7 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   data = req.body;
+  saveData();
   res.send(data);
 });
 
