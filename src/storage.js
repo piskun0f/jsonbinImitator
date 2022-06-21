e42347dcc7e0fdee1909776b9a65c6c018adf2ea
Андрей Пискунов <adpiskunov@miem.hse.ru>
2022-06-21 16:07:00 +0300

    🥅 Add catch errors

--- src/storage.js
+++ src/storage.js
@@ -24,8 +24,12 @@ module.exports = class Storage {
   }
 
   backupData() {
-    fs.writeFileSync(path.join("backups", FILE_NAME + Date.now().toString()) + FILE_EXTENSION, JSON.stringify(this._data), { encodin: "utf-8" })
-    logger.info("Backup done")
+    if (this._data) {
+      fs.writeFileSync(path.join("backups", FILE_NAME + Date.now().toString()) + FILE_EXTENSION, JSON.stringify(this._data), { encodin: "utf-8" })
+      logger.info("Backup done")
+      return;
+    }
+    logger.warning("Can't backup. Data is empty")
   }
 
   getData() {
